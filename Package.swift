0526f8434cfed4f314218bdf239073632972168b
d_abe <abe@flup.jp>
2016-02-26 12:50:36 +0900

    CLionプロジェクト作成

---
+++ Package.swift
@@ -0,0 +1,11 @@
+import PackageDescription
+
+let package = Package(
+    name: "kitura_sample",
+    dependencies: [
+            .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", majorVersion: 0),
+    ],
+    testDependencies: [
+            .Package(url: "https://github.com/kylef/spectre-build.git", majorVersion: 0),
+    ]
+)
